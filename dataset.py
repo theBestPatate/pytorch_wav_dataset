39052a3a33ed698eac1a8dc05b7c1c83f90c67bd
theBestPatate <jean-pierre.gninahophin@etu.univ-amu.fr>
2025-07-09 07:43:06 +0000

    Added: - Backend control - Possiblity to apply a transform Removed: - Useless dependencies

--- dataset.py
+++ dataset.py
@@ -1,5 +1,5 @@
-from torch.utils.data import Dataset, DataLoader
-from torchaudio import transforms, utils, load
+from torch.utils.data import Dataset
+from torchaudio import load
 from pathlib import Path
 
 
@@ -8,15 +8,17 @@ class wavDataset(Dataset):
     An extension of the pytorch Dataset for a generic wav Dataset.
     """
 
-    def __init__(self, wavPath: Path | str, transform: callable = None):
+    def __init__(self, wavPath: Path | str, transform: callable = None, backend=None):
         """
         Arguments:
             wavPath (Path | str): Path to the audio dir with all the wav files
             transform (callable , optional): Optional transformation to be applied on a sample
+            backend (str , optional): ["ffmpeg", "sox", "soundfile"]
         """
         self.wavPath = Path(wavPath)
         self.transform = transform
         self._wav_list = None
+        self.backend = backend
 
     def __len__(self):
         """
@@ -29,10 +31,10 @@ class wavDataset(Dataset):
         if self._wav_list is None:
             self._set_wav_files()
         wav_file_path = self._wav_files[idx]
-        return (
-            *load(wav_file_path, normalize=True, backend="soundfile"),
-            wav_file_path,
-        )
+        audio, sample_rate = load(wav_file_path, normalize=True, backend=None)
+        if self.transform:
+            audio = self.transform(audio)
+        return (audio, sample_rate, wav_file_path)
 
     def _set_wav_files(self):
         """Retrieve and cache the sorted list of .wav files."""
