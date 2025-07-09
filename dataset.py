bd28cc76114591a95dfb820e46e3f2c881ecf60a
theBestPatate <jean-pierre.gninahophin@etu.univ-amu.fr>
2025-07-09 06:13:27 +0000

    init off the wavDataset classes with type hinting,docstrings, and initialisation

--- dataset.py
+++ dataset.py
@@ -8,8 +8,14 @@ class wavDataset(Dataset):
     An extension of the pytorch Dataset for a generic wav Dataset.
     """
 
-    def __init__(self, wavPath: Path, transform: callable = None):
-        pass
+    def __init__(self, wavPath: Path | str, transform: callable = None):
+        """
+        Arguments:
+            wavPath (Path | str): Path to the audio dir with all the wav files
+            transform (callable,optional): Optional transformation to be applied on a sample
+        """
+        self.wavPath = wavPath
+        self.transform = transform
 
     def __len__(self):
         pass
