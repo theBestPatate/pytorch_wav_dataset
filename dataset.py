9433728c57e8c11624927371c0034167d5df079b
theBestPatate <jean-pierre.gninahophin@etu.univ-amu.fr>
2025-07-09 05:56:16 +0000

    Dataset boilerplate

--- dataset.py
+++ dataset.py
@@ -1,2 +1,17 @@
 from torch.utils.data import Dataset, DataLoader
 from torchaudio import transforms, utils
+
+
+class wavDataset(Dataset):
+    """
+    An extension of the pytorch Dataset for a generic wav Dataset.
+    """
+
+    def __init__(self, wavPath, transform=None):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self, idx):
+        pass
