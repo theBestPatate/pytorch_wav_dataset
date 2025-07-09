c0b6b189e0ca90a706682c2aa6b764c1d0787cf5
theBestPatate <jean-pierre.gninahophin@etu.univ-amu.fr>
2025-07-09 06:31:38 +0000

    Convert the wavPath to a Pathlib's path an intialisation Added a method to cache the list of files Used the new method to make the __getitem__ slightly more efficient

--- dataset.py
+++ dataset.py
@@ -14,15 +14,22 @@ class wavDataset(Dataset):
             wavPath (Path | str): Path to the audio dir with all the wav files
             transform (callable,optional): Optional transformation to be applied on a sample
         """
-        self.wavPath = wavPath
+        self.wavPath = Path(wavPath)
         self.transform = transform
+        self._wav_list = None
 
     def __len__(self):
         """
         The method that returns the length of the dataset.
         It returns the number of wav files at wavPath with naive globbing"
         """
-        return len(Path(self.wavPath).glob("*.wav"))
+        return len(self.wavPath.glob("*.wav"))
 
     def __getitem__(self, idx):
-        pass
+        if self._wav_list is None:
+            self._set_wav_files()
+        return len(self._wav_list)
+
+    def _set_wav_files(self):
+        """Retrieve and cache the list of .wav files."""
+        self._wav_files = list(self.wavPath.glob("*.wav"))
